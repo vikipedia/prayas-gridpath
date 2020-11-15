3d7a40f61d5789d441a987f70236a0d62cd4d009
vikipedia <>
2020-11-16 00:28:36 +0530

    changes to handle scenario1 with daily timepoints.

--- src/hydro_opchars.py
+++ src/hydro_opchars.py
@@ -55,7 +55,17 @@ def get_balancing_type(webdb, scenario):
                             "balancing_type_horizon",
                             temporal_scenario_id=temporal_scenario_id)
 
-def get_power_mw(webdb, scenario, project):
+
+def get_temporal_start_end_table(conn, scenario):
+    temporal_id = get_temporal_scenario_id(conn, scenario)
+    temporal = conn.where("inputs_temporal_horizon_timepoints_start_end",
+                          temporal_scenario_id=temporal_id).list()
+    return temporal
+
+
+
+
+def get_power_mw_dataset(webdb, scenario, project):
     scenario_id = common.get_field(webdb,
                                    'scenarios',
                                    "scenario_id",
@@ -64,7 +74,8 @@ def get_power_mw(webdb, scenario, project):
                        scenario_id=scenario_id,
                        project=project,
                        operational_type='gen_hydro').list()
-    return pd.DataFrame(rows)['power_mw']
+    
+    return pd.DataFrame(rows)
 
 
 def adjust_mean_const(b, min_, max_):
@@ -78,23 +89,23 @@ def adjust_mean_const(b, min_, max_):
             extra = c[c > max_] - max_[c > max_]
             c1[c > max_] = max_[c > max_]
             c1[c < min_] += extra.sum()/(c<min_).sum()
-            print(c1.mean(),c.mean())
+            #print(c1.mean(),c.mean())
             c = c1.copy()
         if (c > min_).sum():
             extra = min_[c < min_] - c[c < min_]
             c1[c < min_] = min_[c < min_]
             c1[c > min_] -= extra.sum()/(c>min_).sum()
-            print(c1.mean(),c.mean())
+            #print(c1.mean(),c.mean())
         
         return c1
     
     c1 = adjust(b)
-    printcols(c1, min_, max_)
+    #printcols(c1, min_, max_)
     n = 0
     while n <10 and not np.all((c1 >= min_) & (c1 <= max_)):
-        print(f"iteration {n}..")
+        #print(f"iteration {n}..")
         c1 = adjust(c1)
-        printcols(c1, min_, max_)
+        #printcols(c1, min_, max_)
     
 
     return c1
@@ -117,16 +128,34 @@ def get_projects(webdb, scenario):
     return [row['project'] for row in rows]
 
 
+
+
+def reduce_size(webdb, df, scenario):
+    tmp1 = get_temporal_start_end_table(webdb, scenario)
+
+    horizon = [] 
+    for row in df.to_dict(orient="records"):
+        x = row['timepoint']
+        horizon.append([p['horizon'] for p in tmp1 if x >= p['tmp_start'] and x <= p['tmp_end']][0])
+    df['horizon'] = horizon
+    
+    grouped = df.groupby('horizon').mean()
+    grouped.reset_index(inplace=True, drop=True)
+    return grouped
+
 def adjusted_mean_results(webdb, scenario1, scenario2, project):
     cols = ["balancing_type_project", "horizon", "period",
             "average_power_fraction","min_power_fraction", "max_power_fraction"]
     df = hydro_op_chars_inputs(webdb, scenario2, project)
-    power_mw = get_power_mw(webdb, scenario1, project)
+    power_mw_df = get_power_mw_dataset(webdb, scenario1, project)
     capacity = get_capacity(webdb, scenario1, project)
-    cuf =  power_mw/capacity
-    print(df.columns)
+    cuf =  power_mw_df['power_mw']/capacity
+    #print(df.columns)
     min_, max_ = [df[c] for c in cols[-2:]]
-    printcols(cuf, min_, max_)
+    #printcols(cuf, min_, max_)
+    if len(cuf) > len(min_):
+        power_mw_df = reduce_size(webdb, power_mw_df, scenario2)
+        cuf = power_mw_df['power_mw']/capacity
     avg = adjust_mean_const(cuf, min_, max_)
     results = df[cols]
     del results['average_power_fraction']
@@ -177,8 +206,6 @@ def hydro_op_chars(scenario1,
     for project in projects:
         print(f"Computing data for {project}")
         subscenario_id = get_hydro_ops_chars_sceanario_id(webdb, scenario2, project)
-        print(subscenario, subscenario_id, "*"*10)
-        print("hydro_operational_chars_scenario_id =",subscenario_id)
 
         results = adjusted_mean_results(webdb, scenario1, scenario2, project)
         write_results_csv(results,
@@ -186,13 +213,15 @@ def hydro_op_chars(scenario1,
                           subscenario,
                           subscenario_id,
                           csv_location)
+        """
         common.update_subscenario_via_gridpath(subscenario,
                                                subscenario_id,
                                                project,
                                                csv_location,
                                                database,
                                                gridpath_rep)
-
+        """
+        
 @click.command()
 @click.option("-s1", "--scenario1", default="toy1_pass1", help="Name of scenario1")
 @click.option("-s2", "--scenario2", default="toy1_pass2", help="Name of scenario2")
