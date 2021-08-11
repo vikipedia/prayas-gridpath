vikipedia/prayas-gridpath

commit 5a8d21311536ad4db16c1f1e6154b6cac79657fd

Readabiilty and version changes
- Add detailed description in README
- Allow later versions of gridpath
- Some coding style/cosmetic changes in source

src/hydro_opchars.py
@@ -19,8 +19,8 @@ def hydro_op_chars_inputs_(webdb, project,
 
 
 def hydro_op_chars_inputs(webdb, scenario, project):
-    hydro_op_chars_scenario_id = get_hydro_ops_chars_sceanario_id(webdb,
-                                                                  scenario, project)
+    hydro_op_chars_scenario_id = get_hydro_ops_chars_scenario_id(webdb,
+                                                                 scenario, project)
     balancing_type_project = get_balancing_type(webdb, scenario)
     return hydro_op_chars_inputs_(webdb, project,
                                   hydro_op_chars_scenario_id,
@@ -84,7 +84,7 @@ def get_power_mw_dataset(webdb, scenario, project):
 def adjust_mean_const(b, min_, max_):
     """
     adjusts values in b such that original average of b remains as it is
-    but every value of b lied between corresponding min_ and max_
+    but every value of b lies between corresponding min_ and max_
     """
     def adjust(c):
         c1 = c.copy()
@@ -102,7 +102,6 @@ def adjust_mean_const(b, min_, max_):
             #print("-"*5)
             c1[between] -= (min_[less] - c1[less]).sum()/between.sum()
             c1[less] = min_[less]
-        
 
         #print(c.mean(), c1.mean())
         return c1
@@ -172,7 +171,7 @@ def adjusted_mean_results(webdb, scenario1, scenario2, project):
     return results
 
 
-def get_hydro_ops_chars_sceanario_id(webdb, scenario, project):
+def get_hydro_ops_chars_scenario_id(webdb, scenario, project):
     pocs_id = common.get_field(webdb,
                                "scenarios",
                                "project_operational_chars_scenario_id",
@@ -201,14 +200,14 @@ def write_results_csv(results,
     return subscenario, subscenario_id
 
 
-def hydro_op_chars(scenario1,
-                   scenario2,
+def hydro_op_chars(database,
                    csv_location,
-                   database,
-                   gridpath_rep,
+                   gridpath_repo,
+                   scenario1,
+                   scenario2,
+                   description,
                    project,
-                   update_database,
-                   description):
+                   update_database):
     webdb = common.get_database(database)
     projects = get_projects(webdb, scenario1)
     if project:
@@ -217,7 +216,7 @@ def hydro_op_chars(scenario1,
     subscenario = "hydro_operational_chars_scenario_id"
     for project_ in projects:
         print(f"Computing data for {project_}")
-        subscenario_id = get_hydro_ops_chars_sceanario_id(webdb, scenario2, project_)
+        subscenario_id = get_hydro_ops_chars_scenario_id(webdb, scenario2, project_)
 
         results = adjusted_mean_results(webdb, scenario1, scenario2, project_)
         write_results_csv(results,
@@ -232,35 +231,35 @@ def hydro_op_chars(scenario1,
                                                    project_,
                                                    csv_location,
                                                    database,
-                                                   gridpath_rep)
+                                                   gridpath_repo)
         
         
 @click.command()
+@click.option("-d", "--database", default="../toy.db", help="Path to database")
+@click.option("-c", "--csv_location", default="csvs_toy", help="Path to folder where csvs are")
+@click.option("-g", "--gridpath_repo", default="../", help="Path of gridpath source repository")
 @click.option("-s1", "--scenario1", default="toy1_pass1", help="Name of scenario1")
 @click.option("-s2", "--scenario2", default="toy1_pass2", help="Name of scenario2")
-@click.option("-c", "--csv_location", default="csvs_toy", help="Path to folder where csvs are")
-@click.option("-d", "--database", default="../toy.db", help="Path to database")
-@click.option("-g", "--gridpath_rep", default="../", help="Path of gridpath source repository")
+@click.option("-m", "--description", default="rpo50S3_all", help="Description for csv files.")
 @click.option("--project", default=None, help="Run for only one project")
 @click.option("--update_database/--no-update_database", default=False, help="Update database only if this flag is True")
-@click.option("-m", "--description", default="rpo50S3_all", help="Description for csv files.")
-def main(scenario1,
-         scenario2,
+def main(database,
          csv_location,
-         database,
-         gridpath_rep,
+         gridpath_repo,
+         scenario1,
+         scenario2,
+         description,
          project,
-         update_database,
-         description
+         update_database
          ):
 
-    hydro_op_chars(scenario1,
-                   scenario2,
+    hydro_op_chars(database,
                    csv_location,
-                   database,
-                   gridpath_rep,
-                   project,
+                   gridpath_repo,
+                   scenario1,
+                   scenario2,
                    update_database,
+                   project,
                    description)
 
 def dbtest():    
@@ -288,9 +287,9 @@ def test_1():
 def test_compare_with_db():
 
     def get_db_results():
-        subscenario_id = get_hydro_ops_chars_sceanario_id(webdb,
-                                                          compare_scenario,
-                                                          project)
+        subscenario_id = get_hydro_ops_chars_scenario_id(webdb,
+                                                         compare_scenario,
+                                                         project)
         rows = webdb.where("inputs_project_hydro_operational_chars",
                            project=project,
                            hydro_operational_chars_scenario_id=subscenario_id).list()
@@ -309,7 +308,7 @@ def test_compare_with_db():
     webdb = common.get_database(database)
     subscenario = "hydro_operational_chars_scenario_id"
     print(f"Computing data for {project}")
-    subscenario_id = get_hydro_ops_chars_sceanario_id(webdb, scenario2, project)
+    subscenario_id = get_hydro_ops_chars_scenario_id(webdb, scenario2, project)
     results = adjusted_mean_results(webdb, scenario1, scenario2, project)
     write_results_csv(results,
                       project,

src/endogenous_exogenous.py
@@ -297,7 +297,7 @@ def write_exogenous_via_gridpath_script(scenario1,
                                         fo,
                                         project,
                                         csv_location,
-                                        gridpath_rep,
+                                        gridpath_repo,
                                         db_path,
                                         description,
                                         update_database):
@@ -323,7 +323,7 @@ def write_exogenous_via_gridpath_script(scenario1,
     if update_database and sanity_check(csvpath):
         common.update_subscenario_via_gridpath(subscenario, subscenario_id,
                                                project, csv_location, db_path,
-                                               gridpath_rep)
+                                               gridpath_repo)
 
     
 def find_projects(scenario1, type_, webdb):
@@ -352,10 +352,10 @@ def endogenous_to_exogenous(scenario1:str,
                             fo:str,
                             csv_location:str,
                             database:str,
-                            gridpath_rep:str,
+                            gridpath_repo:str,
                             skip_scenario2:bool,
                             project:str,
-                            description:str,
+                            name:str,
                             update_database:bool):
 
     projs = find_projects_to_copy(scenario1, scenario2, database)
@@ -371,9 +371,9 @@ def endogenous_to_exogenous(scenario1:str,
                                                 fo=None,
                                                 project=project_,
                                                 csv_location=csv_location,
-                                                gridpath_rep= gridpath_rep,
+                                                gridpath_repo= gridpath_repo,
                                                 db_path=database,
-                                                description=description,
+                                                description=name,
                                                 update_database=update_database)
 
     if scenario3:
@@ -382,7 +382,8 @@ def endogenous_to_exogenous(scenario1:str,
             fo = pd.read_excel(fo,
                                sheet_name="gridpath-input",
                                nrows=35041,
-                               usecols=projs)
+                               usecols=projs,
+                               engine='openpyxl')
         
         for project_ in projs:
             print(f"Starting {project_} for {scenario3} ...")
@@ -392,57 +393,65 @@ def endogenous_to_exogenous(scenario1:str,
                                                 fo,
                                                 project_,
                                                 csv_location,
-                                                gridpath_rep,
+                                                gridpath_repo,
                                                 db_path=database,
-                                                description=description,
+                                                description=name,
                                                 update_database=update_database)
 
 
 @click.command()
-@click.option("-s1", "--scenario1", default="toy1_pass1", help="Name of scenario1")
-@click.option("-s2", "--scenario2", default="toy1_pass2", help="Name of scenario2")
-@click.option("-s3", "--scenario3", default=None, help="Name of scenario3")
-@click.option("-f", "--fo", default=None, help="Excel filepath, containing forced outage information")
-@click.option("-c", "--csv_location", default="csvs_toy", help="Path to folder where csvs are")
-@click.option("-d", "--database", default="../toy.db", help="Path to database")
-@click.option("-g", "--gridpath_rep", default="../", help="Path of gridpath source repository")
-@click.option("--skip_scenario2/--no-skip_scenario2", default=False, help="skip copying for senario2")
-@click.option("--project", default=None, help="Run only for one project")
-@click.option("-m", "--description", default="rpo50S3_all", help="Description for csv files.")
-@click.option("--update_database/--no-update_database", default=False, help="Update database only if this flag is True")
+@click.option("-d", "--database", default="dispatch.db", help="Path to database (default: dispatch.db")
+@click.option("-c", "--csv_location", default="csvs", help="Path to folder where csvs are (default: csvs)")
+@click.option("-g", "--gridpath_repo", default="..", help="Path of gridpath source repository (default: ..)")
+@click.option("-s1", "--scenario1", default="pass1", help="Name of scenario1 (default: pass1)")
+@click.option("-s2", "--scenario2", default="pass2", help="Name of scenario2 (default: pass2)")
+@click.option("-s3", "--scenario3", default=None, help="Name of scenario3 (default: None)")
+@click.option("-f", "--fo", default=None, help="Excel filepath, containing forced outage information (default: None)")
+@click.option("--skip_scenario2/--no-skip_scenario2", default=False, help="skip copying for senario2 (default: no-skip)")
+@click.option("--project", default=None, help="Run only for one project (default: None")
+@click.option("-n", "--name", default="all", help="Description in name of csv files (default: all)")
+@click.option("--update_database/--no-update_database", default=False, help="Update database only if this flag is True (default: no-update)")
 def main(scenario1:str,
          scenario2:str,
          scenario3:str,
          fo:str,
          csv_location:str,
          database:str,
-         gridpath_rep:str,
+         gridpath_repo:str,
          skip_scenario2:bool,
          project:str,
-         description:str,
+         name:str,
          update_database:bool):
 
     """
-    Usage: python endogenous_exogenous.py [OPTIONS]
+    Usage: python availability.py [OPTIONS]
     this is a script to copy endogenous output from scenario1
     to exogenous input of scenario2. to run this script, gridpath
     virtual environment must be active.
 
     Options:
 
-      --scenario1 TEXT     default -> toy1_pass1
+      --database TEXT       default -> ../toy.db
 
-      --scenario2 TEXT     default -> toy1_pass2
+      --csv_location TEXT   default -> csvs_toy
 
-      --scenario3 TEXT     default -> None
+      --gridpath_repo TEXT  default-> ../
 
-      --fo TEXT  default -> None     
+      --scenario1 TEXT      default -> toy1_pass1
 
-      --csv_location TEXT      default -> csvs_toy
+      --scenario2 TEXT      default -> toy1_pass2
 
-      --database TEXT      default -> ../toy.db
+      --scenario3 TEXT      default -> None
 
-      --gridpath_rep TEXT  default-> ../
+      --fo TEXT             default -> None     
+
+      --skip_scenario2      default -> no-skip_scenario2
+
+      --project TEXT        default -> None
+
+      --name TEXT           default -> all
+
+      --update_database     default -> no-update_database
 
     """
     return endogenous_to_exogenous(
@@ -452,10 +461,10 @@ def main(scenario1:str,
         fo,
         csv_location,
         database,
-        gridpath_rep,
+        gridpath_repo,
         skip_scenario2,
         project,
-        description,
+        name,
         update_database
     )
     

src/forced_outage.py
@@ -6,25 +6,22 @@ import collections
 
 def generate_fo(length, average, maxoutage, minoutage):
     pass
-    
 
 
-def combine_fo_m(m, moved_f):
+def combine_fo_m(m, f):
     """    
     derate
     1 -> available
     0 -> not available
 
-    rules for combing after moving fo
+    rules for combining after moving fo
     r -> min(m,fo)
-
     """
-    df = pd.DataFrame({"m": m,
-                       "newf": moved_f})
+    df = pd.DataFrame({"m": m, "newf": moved_f})
     return df.apply(min, axis=1)
 
 
-def moved_fo(m, f):
+def move_fo(m, f):
     """
     m      f
     <1     <1    move fo where m is 1
@@ -195,54 +192,54 @@ def prints(s):
 def test_moved_fo():
     m = str_to_series("111100011111111")
     f = str_to_series("111100011111111")
-    nf = moved_fo(m, f)
+    nf = move_fo(m, f)
     check_asserts(m, f, nf)
     
     m = str_to_series("111100011111111")
     f = str_to_series("111110001111111")
-    nf = moved_fo(m, f)
+    nf = move_fo(m, f)
     check_asserts(m, f, nf)
     
     m = str_to_series("111100011111111")
     f = str_to_series("110001111111111")
-    nf = moved_fo(m, f)
+    nf = move_fo(m, f)
     check_asserts(m, f, nf)
     
     m = str_to_series("111100011111111")
     f = str_to_series("110001111111111")
-    nf = moved_fo(m, f)
+    nf = move_fo(m, f)
     check_asserts(m, f, nf)
 
     
     m = str_to_series("111100011111111000")
     f = str_to_series("110001111111100011")
-    nf = moved_fo(m, f)
+    nf = move_fo(m, f)
     check_asserts(m, f, nf)
 
 
     m = str_to_series("111100011111111111")
     f = str_to_series("111110000001100000")
-    nf = moved_fo(m, f)
+    nf = move_fo(m, f)
     check_asserts(m, f, nf)
     
     m = str_to_series("111100011111111")
     f = str_to_series("111110000001111")
-    nf = moved_fo(m, f)
+    nf = move_fo(m, f)
     check_asserts(m, f, nf)
 
     m = str_to_series("111100011000000")
     f = str_to_series("111110000001111")
-    nf = moved_fo(m, f)
+    nf = move_fo(m, f)
     check_asserts(m, f, nf)
 
     m = str_to_series("110001111000000")
     f = str_to_series("111000000111111")
-    nf = moved_fo(m, f)
+    nf = move_fo(m, f)
     check_asserts(m, f, nf)
 
     m = str_to_series("1111110000111111")
     f = str_to_series("1100111111111000")
-    nf = moved_fo(m, f)
+    nf = move_fo(m, f)
     check_asserts(m, f, nf)
 
 

commit d3b1ff96ff65631d547c8549474def98257e2c7f

Fix incorrect order in parameters passed to hydro_op_chars()
Introduced in commit 5a8d21311536ad4db16c1f1e6154b6cac79657fd

src/hydro_opchars.py
@@ -258,9 +258,9 @@ def main(database,
                    gridpath_repo,
                    scenario1,
                    scenario2,
-                   update_database,
+                   description,
                    project,
-                   description)
+                   update_database)
 
 def dbtest():    
     webdb = common.get_database("/home/vikrant/programming/work/publicgit/gridpath/mh.db")