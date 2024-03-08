72a7ef4c4d430dd367fc410b24ba6d3746897d06
vikipedia <>
2024-03-08 17:26:17 +0530

    added timestamp to log file name

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,8 @@
 import logging
 import logging.handlers
 import os
+import datetime
+
 
 levels = {"INFO": logging.INFO,
           "WARN": logging.WARN,
@@ -8,13 +10,22 @@ levels = {"INFO": logging.INFO,
           "ERROR": logging.ERROR}
 
 
-def init(LOGFILENAME, levelname="INFO"):
+def get_log_filepath(prefix):
+    location = os.getcwd()
+    format_ = "%Y-%m-%d-%H-%M-%S"
+    timestamp = datetime.datetime.now().strftime(format_)
+    return os.path.join(location, ".".join(["rumi", prefix, timestamp, "log"]))
+
+
+def init(prefix, levelname="INFO"):
     if os.getenv("DEBUG") == "True":
         print("Setting log level to DEBUG")
         levelname = "DEBUG"
 
     formatter = logging.Formatter(
         '%(asctime)s %(threadName)-10s %(name)s %(levelname)-8s %(message)s')
+    LOGFILENAME = get_log_filepath(prefix)
+    print("Redirecting log to", LOGFILENAME)
     handler = logging.FileHandler(filename=LOGFILENAME, mode="w")
     handler.setFormatter(formatter)
     handler.setLevel(levels[levelname])
